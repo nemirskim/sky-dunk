b37e26a8bfc0021501e60e8b1eb3293b4b1436a8
kot lena <aternetas@gmail.com>
2023-09-01 23:10:11 +0300

    add Observable class

--- app/src/main/java/com/example/skydunk/UI/main/MainActivity.kt
+++ app/src/main/java/com/example/skydunk/UI/main/MainActivity.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.skydunk.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    var viewModel: MainViewModel = MainViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        viewModel.name.bind {
+            binding.teamNameTextView.text = it
+        }
     }
 }
---
+++ app/src/main/java/com/example/skydunk/UI/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.skydunk.UI.main
+
+import com.example.skydunk.system.Observable
+
+class MainViewModel {
+    var name = Observable("Boston Celtics")
+}
